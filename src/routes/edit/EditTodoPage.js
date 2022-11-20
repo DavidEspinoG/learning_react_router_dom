d153ab2325f6174bc305b8be1e5cc0576c7b4ff5
David Espino <davidespinog@gmail.com>
2022-11-19 18:32:06 -0600

    session twenty finished

---
+++ src/routes/edit/EditTodoPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function EditTodoPage() {
+    return (
+        <p>Editar todo</p>
+    )
+}
+export { EditTodoPage }
---
+++ src/routes/new/NewTodoPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function NewTodoPage() {
+    return (
+        <p>New Todo Page</p>
+    )
+}
+export { NewTodoPage }
